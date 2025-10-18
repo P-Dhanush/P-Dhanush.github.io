42c60e1674db5af029ae4da03559fc745e71ea68
Dhanush <dhanushpamulapati@gmail.com>
2025-10-18 22:34:47 +0530

    add built site files for github pages

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Testing All The Features",
+        "excerpt":"My First Post: A Feature Test This post is a demonstration of all the formatting capabilities available in a GitHub Pages blog using Jekyll and Markdown. We will cover everything from basic text formatting to advanced embeds. This text is bold and this text is italic. It’s easy to add...","categories": ["notes"],
+        "tags": ["jekyll","howto"],
+        "url": "/notes/2015/10/13/my-feature-test.html",
+        "teaser": null
+      },{
+        "title": "Column Store (SoA)",
+        "excerpt":"Crypto L2 Analysis: Part 1 - Column Store Architecture Crypto L2 Analysis: Part 1 - Column Store Architecture Workflow Directory Structure Strategy Obtaining the parameters involved. (Read Stage) Writing out the parameters per our structure. The actual code used in each file: colstore.hpp ndjson_okx.hpp okx_to_colstore.cpp Workflow Taking OKX L2 Data...","categories": ["notes"],
+        "tags": ["jekyll","howto"],
+        "url": "/notes/2025/10/15/Column-Store-(SoA).html",
+        "teaser": null
+      },{
+        "title": "WSL",
+        "excerpt":"title: “WSL Notes” description: “A living document of how WSL and practical tips to use it.” last_updated: 2025-10-15 tags: [wsl, linux, windows, bash, powershell, dev-setup] — WSL Notes A structured, evolving reference for understanding WSL (Windows Subsystem for Linux) from fundamentals to daily workflows, including Bash vs PowerShell, WSL1 vs...","categories": ["notes"],
+        "tags": ["jekyll","howto"],
+        "url": "/notes/2025/10/15/WSL.html",
+        "teaser": null
+      },{
+        "title": "CPP Learnings",
+        "excerpt":"CPP - Learnings &amp; Notes CPP - Learnings &amp; Notes Reading the Language (hopefully like a professional) Value Categories Lifetime &amp; Storage ODR One Definition Rule Object Model &amp; Memory Layouts and Invariants Ownerships vs Views Non-owning Views: string_view, span Small Buffer Optimizations (SBO) Allocators Constructor, Initialization and Destruction Member-Initializer...","categories": ["notes"],
+        "tags": ["cpp"],
+        "url": "/notes/2025/10/15/cpplearnings.html",
+        "teaser": null
+      },{
+        "title": "C++ Object Model & RAII (Ownership, Lifetime, Memory)",
+        "excerpt":"# 1. Types &amp; Layout 1.1 Classes &amp; Structs 1.2 Data members, access-control, standard layout vs trivial 1.2 Data Members, Access Control, Standard-Layout vs Trivial Access Control (public/protected/private) 1.3 static members (type-level) vs non-static (object-level) 2. Object Model 3. Construction &amp; Destruction (RAII Core) 4. Initialization patterns 5. Ownership buffers...","categories": ["notes"],
+        "tags": ["cpp"],
+        "url": "/notes/2025/10/15/object-model-raii.html",
+        "teaser": null
+      },{
+        "title": "WSL Cheatsheet",
+        "excerpt":"WSL Cheatsheet — From First Principles to Power User WSL Cheatsheet — From First Principles to Power User 1. First Principles 1.1 OS, Kernel, Userspace 1.2 Where WSL fits 1.3 WSL1 vs WSL2 (quick compare) 2. Shells &amp; Environments 2.1 Bash (Linux) vs PowerShell (Windows) 2.2 Profiles 2.3 Environment Variables...","categories": ["notes"],
+        "tags": ["wsl","linux","windows","bash","powershell","dev-setup","jekyll"],
+        "url": "/notes/2025/10/15/WSLCheatsheet.html",
+        "teaser": null
+      },{
+        "title": "Pointers & References",
+        "excerpt":"Pointers &amp; References Pointers &amp; References Pointer Indirection Optionality Polymorphism Object Slicing Porblem without Pointers (loses derived info) Copying vs Moving When to use Pointers Dynamic Allocation (heap memory) Linked Data Structures (Trees, Graphs, etc.) Polymorpism (Dynamic Dispatch) Avoiding Copies (Performance &amp; Semantics) {REFERENCES} Reference? What is it? Reference to...","categories": ["notes"],
+        "tags": ["cpp"],
+        "url": "/notes/2025/10/16/pointers_and-references.html",
+        "teaser": null
+      },{
+        "title": "Classes",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/2025/10/17/classes.html",
+        "teaser": null
+      },{
+        "title": "Structs",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/2025/10/17/structs.html",
+        "teaser": null
+      }]
